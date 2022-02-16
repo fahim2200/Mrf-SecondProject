51c553a5798a600ad8412a6f7fc1d70a63a1b417
fahim2200 <fahimrahman223@gmail.com>
2022-02-16 13:06:51 +0600

    Laravel Day 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('master')
+
+@section('title')
+   About Page
+
+@endsection
+@section('body')
+    <h1>About Page</h1>
+
+@endsection
+
+
+{{--<h1>This is ABOUT page....</h1>--}}
+
+{{--<a href="{{ route('home') }}"> Home </a>--}}
+{{--<a href="{{ route('bitm') }}"> BITM </a>--}}
+{{--<a href="{{ route('about') }}"> About </a>--}}
+{{--<a href="{{ route('contact') }}"> Contact </a>--}}
---
+++ resources/views/demo.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+
+@section('title')
+    BITM Page
+
+@endsection
+@section('body')
+    <h1>BITM Page</h1>
+
+@endsection
+{{--<a href="{{ route('home') }}"> Home </a>--}}
+{{--<a href="{{ route('bitm') }}"> BITM </a>--}}
+{{--<a href="{{ route('about') }}"> About </a>--}}
+{{--<a href="{{ route('contact') }}"> Contact </a>--}}
+
